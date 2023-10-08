afeadfc20c37863a494cecf715a4441cc5a9f5cc
marinight <marinight17@gmail.com>
2023-10-07 23:20:12 -0300

    fix formatting PolarCoordinates.hx

--- src/numerics/PolarCoordinates.hx
+++ src/numerics/PolarCoordinates.hx
@@ -1,31 +1,38 @@
 package numerics;
 
-typedef PolarCoordinatesType = {
+typedef PolarCoordinatesType =
+{
 	var magnitude:Float;
 	var phaseAngle:Float;
 }
 
-abstract PolarCoordinates(PolarCoordinatesType) from PolarCoordinatesType to PolarCoordinatesType {
+abstract PolarCoordinates(PolarCoordinatesType) from PolarCoordinatesType to PolarCoordinatesType 
+{
 	public var magnitude(get, set):Float;
 	public var phaseAngle(get, set):Float;
 
-	inline function get_magnitude():Float {
+	inline function get_magnitude():Float
+	{
 		return this.magnitude;
 	}
 
-	inline function set_magnitude(n:Float):Float {
+	inline function set_magnitude(n:Float):Float
+	{
 		return this.magnitude = n;
 	}
 
-	inline function get_phaseAngle():Float {
+	inline function get_phaseAngle():Float
+	{
 		return this.phaseAngle;
 	}
 
-	inline function set_phaseAngle(n:Float):Float {
+	inline function set_phaseAngle(n:Float):Float
+	{
 		return this.phaseAngle = n;
 	}
 
-	public function new(magnitude:Float, phaseAngle:Float) {
+	public function new(magnitude:Float, phaseAngle:Float)
+	{
 		this = {magnitude: magnitude, phaseAngle: phaseAngle};
 	}
 }
